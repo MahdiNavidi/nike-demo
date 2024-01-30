aef961c69a18eae6e62734fba29dfed3f73b65cd
mahdi <navidi.mahdi@gmail.com>
2024-01-30 09:35:46 +0330

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PoplularProducts from "./PoplularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PoplularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality
+}
